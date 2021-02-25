ff025e859ccf70930e7898a67c80599d87ee3126
mind999hacker <75688231+mind999hacker@users.noreply.github.com>
2021-02-24 23:04:38 -0800

    lab 6 done

--- Main.java
+++ Main.java
@@ -1,14 +1,23 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Shahmir Ahmad
 
-//import static java.lang.System.*;
+import static java.lang.System.*;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-Word one = new Word("test");//out.println(one);
-System.out.println("num vowels == " + one.getNumVowels());
+     Word one = new Word("test");
+      one.setWord("alligator");
+    out.println("\n\n"+one);
+     out.println("num vowels == " + one.getNumVowels());
+      out.println("num chars == " + one.getLength());
+
+
+	  one.setWord("elephant");
+out.println("\n\n"+one);
+out.println("num vowels == " + one.getNumVowels());
+out.println("num chars == " + one.getLength());
 	}
 }
--- Word.java
+++ Word.java
@@ -1,44 +1,58 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Shahmir Ahmad
 
 //import static java.lang.System.*;
 
 public class Word
 {
-   //private static String[] vowels = {"a","e","i","o","u"}; 
+   private static Character[] vowels = {'a','e','i','o','u'}; //char array to compare to the letters later on
      	//why is this here?
    																//why is it static?
    
    //add an instance variable
-   private String word = "";
+   private String word = ""; //new string word
 	//create a constructor
-	public Word(String tmp)
+	public Word(String tmp) //new constructor method
   {
-    word = tmp;
+    word = tmp.toLowerCase(); //set word to tmp String lowercased
   }
 	//create other methods
-  public int getNumVowels()
+  public int getNumVowels() //new int method getNumVowels
   {
-    if(word.length == 0)
+    if(word.length() == 0) //check if word.length is == 0 just to make sure if they forget to set a word
     {
-      return -1;
+      return -1; //if yes return -1
     }
 
-    int count = 0;
-    for(int i = 0;i<word.length;i++){
-      for(String x: vowels)
+    int count = 0;  //new int count
+    for(int i = 0;i<word.length();i++){ //loop from 0 till word.length add 1 every loop
+      for(Character x: vowels) //new for each loop iterate thru vowels array
       {
-        if(word.charAt(0).toLowerCase.equals(x))
+        if(word.charAt(i)==x) //get the char at the idex and see if it equals the currunt vowels char
         {
-          count++;
+          count++; //if yes add 1 to count
         }
       }
     }
-    return count;
+    return count; //retrn count
+  }
+
+  public int getLength() //new int getLength method
+  {
+    return word.length(); //return length of the word
+  }
+  public void setWord(String tmp) { //new setWord method 
+    word = tmp.toLowerCase(); //set word to tmp String lowercased
+
   }
 	//look at the sample runner code
 	//on the lab handout
 
 	//create a toString method
+  public String toString() //new toString method
+  {
+    return word; //return word
+  }
+
 }
