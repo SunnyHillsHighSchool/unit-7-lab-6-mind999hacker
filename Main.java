a15a14e181ddf3d0014ce2f8f532dcc6bb31cbe9
Shahmir <shahmirahmed20@gmail.com>
2021-02-25 06:42:30 +0000

    error stuff

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@
 //www.apluscompsci.com
 //Name -
 
-import static java.lang.System.*;
+//import static java.lang.System.*;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
+Word one = new Word("test");//out.println(one);
+System.out.println("num vowels == " + one.getNumVowels());
 	}
 }
--- Word.java
+++ Word.java
@@ -2,18 +2,41 @@
 //www.apluscompsci.com
 //Name -
 
-import static java.lang.System.*;
+//import static java.lang.System.*;
 
 public class Word
 {
-   private static String vowels = "AEIOUaeiou";   	//why is this here?
+   //private static String[] vowels = {"a","e","i","o","u"}; 
+     	//why is this here?
    																//why is it static?
    
    //add an instance variable
-
+   private String word = "";
 	//create a constructor
-	
+	public Word(String tmp)
+  {
+    word = tmp;
+  }
 	//create other methods
+  public int getNumVowels()
+  {
+    if(word.length == 0)
+    {
+      return -1;
+    }
+
+    int count = 0;
+    for(int i = 0;i<word.length;i++){
+      for(String x: vowels)
+      {
+        if(word.charAt(0).toLowerCase.equals(x))
+        {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
 	//look at the sample runner code
 	//on the lab handout
 
